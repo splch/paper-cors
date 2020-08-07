137694e3eb84c1b30693f9729b30616e1cbb3d8e
Spencer Churchill <spencer.l.churchill@gmail.com>
2020-08-06 21:16:09 -0700

    format

--- index.js
+++ index.js
@@ -5,23 +5,22 @@ var port = process.env.PORT || 443;
 
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: ["https://daily-paper.splch.repl.co", "https://daily-paper--splch.repl.co"], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: [
-      'cookie',
-      'cookie2',
-      // Strip Heroku-specific headers
-      'x-heroku-queue-wait-time',
-      'x-heroku-queue-depth',
-      'x-heroku-dynos-in-use',
-      'x-request-start',
-    ],
-    redirectSameOrigin: true,
-    httpProxyOptions: {
-      // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
-      xfwd: false,
-    },
-  }).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-  });
-  
+  originWhitelist: ["https://daily-paper.splch.repl.co", "https://daily-paper--splch.repl.co"], // Allow all origins
+  requireHeader: ['origin', 'x-requested-with'],
+  removeHeaders: [
+    'cookie',
+    'cookie2',
+    // Strip Heroku-specific headers
+    'x-heroku-queue-wait-time',
+    'x-heroku-queue-depth',
+    'x-heroku-dynos-in-use',
+    'x-request-start',
+  ],
+  redirectSameOrigin: true,
+  httpProxyOptions: {
+    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+    xfwd: false,
+  },
+}).listen(port, host, function () {
+  console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
